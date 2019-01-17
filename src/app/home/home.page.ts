4bd83a04619c11a7e48517b7aff32d1397a88f4d
Rafael Yuste <teka_ryj@hotmail.com>
2019-01-17 10:51:00 +0100

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,53 @@
+import { Component,ViewChild} from '@angular/core';
+import { IonSlides, IonInfiniteScroll } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  @ViewChild('SwipedTabsSlider') SwipedTabsSlider: IonSlides;
+
+  private visible = false;
+  private tabs = ["selectTab(0)", "selectTab(1)"];
+  private category: any = "0";
+  private ntabs = 2;
+  private SwipedTabsIndicator: any = null;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  meGusta(){
+    this.visible = !this.visible; 
+  }
+
+  ionViewDidEnter() {
+    this.SwipedTabsIndicator = document.getElementById("indicator");
+  }
+
+  ionViewWillEnter() {
+    this.category = "0";
+    this.SwipedTabsSlider.length().then(l => {  //no sería necesario aquí, solo en ngOnInit
+      this.ntabs = l;
+    });
+  }
+  
+  /* El método que permite actualizar el indicado cuando se cambia de slide*/
+  updateIndicatorPosition() {
+    this.SwipedTabsSlider.getActiveIndex().then(i => {
+      if (this.ntabs > i) {  // this condition is to avoid passing to incorrect index
+        this.SwipedTabsIndicator.style.webkitTransform = 'translate3d(' + (i * 100) + '%,0,0)';
+      }
+    });
+  }
+  /* El método que anima la "rayita" mientras nos estamos deslizando por el slide*/
+  animateIndicator(e) {
+    //console.log(e.target.swiper.progress);
+    if (this.SwipedTabsIndicator)
+      this.SwipedTabsIndicator.style.webkitTransform = 'translate3d(' +
+        ((e.target.swiper.progress * (this.ntabs - 1)) * 100) + '%,0,0)';
+  }
+}
